31aebb63a6f691f1bb2fcbe58484022e5fb6af99
solimm4sks <56252371+solimm4sks@users.noreply.github.com>
2020-02-16 01:14:13 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,3 @@
-  # you can add more profiles here
+  # you can add more profiles here, you should put an actual username and password here
   username = ['username1', 'username2']
   password = ['password1', 'password2']
